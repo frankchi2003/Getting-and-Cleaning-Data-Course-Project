3e6cc36c3af52668a932b7d6daa6ac77f0d5940b
frankchi2003 <chi_frank@comcast.net>
2017-11-28 20:41:15 -0500

    update README.md

--- README.md
+++ README.md
@@ -1,41 +1,37 @@
 # Course Project - Getting and Cleaning Data
 
+The course project for [Getting and Cleaning Data](https://www.coursera.org/course/getdata) read in the ["Human Activity Recognition Using Smartphones" data set](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones), perform an analysis on the data set, and output a tidy data set.
+
 ## Author
 
 Frank Chi <frank.m.chi@gmail.com>
 
 ## Instructions
 
-download and run run\_analysis.R script from within the download directory.
+**You can download and run the R Script ([run_analysis.R](run_analysis.R)) within current working directory.** Note that the script requires **dplyr** and **data.table** packages.
 
-This script will download files described in Data Source and process for generating a tidy data set at `./tidydata.txt`
+**The output of the R script is a tidy data set, [tidydata.txt](tidydata.txt).**
 
-## Data Source
-
-This project uses the "Human Activity Recognition Using Smartphones Dataset". Data set download from:
-	https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
-save to:
-	./data/Dataset.zip by run\_analysis.R from:
+You can read more about the data and the analysis in the [code book](CodeBook.md).
 
 ## Script Walkthrough
 
-The run\_analysis.R script will perform the following steps:
+The R Script ([run_analysis.R](run_analysis.R)) will perform the following steps:
 
-* Ensure working directory 'data' exists
-* Check if archieved data set file exists
-* If archieved file does not exit, downloads it and extracts all files   
-* Reads training and test data set files (Note that these file are stored on 'data/UCI HAR Dataset/train/' and 'data/UCI HAR Dataset/test/' directory)
-* Reads feature names and activity labels (Note that these file are stored on 'data/UCI HAR Dataset/' directory)
+* Create working directory 'data' if it does not exist
+* Download data set archived file from [this URL](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip) and unzip if it has not been downloaded. Note that the zip file will be stored in the new created **'data'** directory and named **'UCI-HAR-Dataset.zip'**.
+* Reads training and test data set files (Note that these files are stored on **'data/UCI HAR Dataset/train/'** and **'data/UCI HAR Dataset/test/'** directory)
+* Reads feature names and activity labels (Note that these files are stored on **'data/UCI HAR Dataset/'** directory)
 * Merges training set and test set to create one data set
 * Extracts measurements on the mean and standard deviation for each measurement
 * Uses descriptive activity names to name the activities in the data set
 * Appropriately labels the data set with descriptive variable names
 * Creates an independent tidy data set with the average of each variable for each activity and each subject
-* Saves the resulting tidy data set to file **_tidydata.txt_** on current working directory
+* Saves the resulting tidy data set to file **'tidydata.txt'** on current working directory
 
 ## Test Environment
 
-The run\_analysis.R script has been tested on following environment:
+The R Script ([run_analysis.R](run_analysis.R)) had been tested on following environment:
 
 RStudio version 1.1.383 - (C) 2009-2017 RStudio, Inc.
 R version 3.4.1 (2017-06-30) -- "Single Candle"
--- run_analysis.R
+++ run_analysis.R
@@ -4,24 +4,27 @@ run_analysis <- function() {
     library(dplyr)
     library(data.table)
 
-    
+    curwd <- getwd()
+
     ## download/unzip the data for the project
     ## project files are stored on local "data" folder
     datadir <- "data"
     if (!file.exists(datadir)) {
+        message(paste("Create data directory on", curwd, "...", sep=" "))
         dir.create(datadir)
     }
     
     ## download project zip file if it doesn't exist
     destfile<- paste(datadir, "UCI-HAR-Dataset.zip", sep="/")
     if (!file.exists(destfile)) {
+        message(paste("Downloading data set archived file to",destfile,"...", sep=" "))
         fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
         download.file(fileUrl, destfile=destfile, mode="wb")
+
         ## unzip the project files
+        message(paste("Extracting the data set files from the archive to",datadir,"...",sep=" "))
         unzip(destfile, exdir=datadir)
-        list.files(datadir, recursive = TRUE)
     }
-    list.files(datadir, recursive = TRUE)
     
     ## Content of project zip file
     ## the zip file contains train dataset and test dataset:
@@ -34,29 +37,36 @@ run_analysis <- function() {
     ## - y_test.txt: test label
     ## - subject_test.txt: identifier of the subject (30 devices) who carried out the experiment
     
+    dsdir <- "data/UCI HAR Dataset"
     ## read train data, lebel and subject
-    X_train <- fread("data/UCI HAR Dataset/train/X_train.txt")
-    Y_train <- fread("data/UCI HAR Dataset/train/y_train.txt")
-    subject_train <- fread("data/UCI HAR Dataset/train/subject_train.txt")
+    message("Reading training data set...")
+    X_train <- fread(paste(dsdir,"train/X_train.txt", sep="/"))
+    Y_train <- fread(paste(dsdir,"train/y_train.txt", sep="/"))
+    subject_train <- fread(paste(dsdir,"train/subject_train.txt", sep="/"))
     
     ## read test data, lable and subject
-    X_test <- fread("data/UCI HAR Dataset/test/X_test.txt")
-    Y_test <- fread("data/UCI HAR Dataset/test/y_test.txt")
-    subject_test <- fread("data/UCI HAR Dataset/test/subject_test.txt")
+    message("Reading test data set...")
+    X_test <- fread(paste(dsdir,"test/X_test.txt", sep="/"))
+    Y_test <- fread(paste(dsdir,"test/y_test.txt", sep="/"))
+    subject_test <- fread(paste(dsdir,"test/subject_test.txt", sep="/"))
     
     ## read feature vector (561-features)
-    feature_names <- fread("data/UCI HAR Dataset/features.txt")
+    message("Reading feature vector...")
+    feature_names <- fread(paste(dsdir,"features.txt", sep="/"))
     
     ## read activity lebels
     ## 6 activities: WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING
-    activity_labels <- fread("data/UCI HAR Dataset/activity_labels.txt")
+    message("Reading activity label...")
+    activity_labels <- fread(paste(dsdir,"activity_labels.txt", sep="/"))
     
     ## 1. Merges the training and the test sets to create one data set
+    message("Mergeing training and test sets...")
     X <- rbind(X_train, X_test)
     Y <- rbind(Y_train, Y_test)
     Subject <- rbind(subject_train, subject_test)
     
     ## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+    message("Extracting mean and std variables...")
     selected_features <- feature_names[grep("mean\\(\\)|std\\(\\)",feature_names$V2),]
     cols = paste("V", selected_features$V1, sep="")
     X <- X[,cols,with=FALSE]
@@ -64,17 +74,23 @@ run_analysis <- function() {
     ## 3. Uses descriptive activity names to name the activities in the data set
     #colnames(Y) <- "activity"
     #Y <- mutate(Y, activityLabel=factor(Y$activity, labels = activity_labels$V2))
+    message("Uses descriptive activity names to name the activities in the data set...")
     activityLabel=factor(Y$V1, labels = activity_labels$V2)
 
     ## 4. Appropriately labels the data set with descriptive variable names.
+    message("Appropriately labels the data set...")
     colnames(X) <- selected_features$V2
     
     ## 5. From the data set in step 4, creates a second, independent tidy data set 
     ##    with the average of each variable for each activity and each subject.
+    message("Createing an independent tidy data set with the average of each variable for each activity and each subject...")
     colnames(Subject) <- "subject"
     final_data <- tbl_df(cbind(Subject, activityLabel, X))
     tidydata <- final_data %>% group_by(activityLabel, subject) %>% summarise_all(mean)
 
+    message("Saving tidy data set to a file...")
     write.table(tidydata, file = "tidydata.txt", row.names = FALSE, col.names = TRUE)
-    write.table(selected_features, file = "tidyfeatures.txt", row.names = FALSE, col.names = FALSE)
+    
+    message("Process Completed.")
+    
 }
